53cdce5269ef7f2df08b844e66f2d9e096ff99ca
psmever <psmever@gmail.com>
2020-02-09 23:31:21 +0900

    쿠키 설정 헬퍼 추가.

--- src/lib/Helper.ts
+++ src/lib/Helper.ts
@@ -36,3 +36,51 @@ export const storageManager = {
         }
     }
 };
+
+
+
+export function removeCookie(cname: string): void {
+	let expires = "expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+	document.cookie = cname + "=;" + expires + ";path=/";
+}
+
+export function setCookie(cname: string, cvalue: string, hours: number = 1): void {
+	let d = new Date();
+	d.setTime(d.getTime() + hours * 60 * 60 * 1000); // (exdays * 24 * 60 * 60 * 1000));
+	let expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+export function getCookie(cname: string): string | null {
+	let name = cname + "=";
+	let ca = document.cookie.split(";");
+
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) === " ") {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) === 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+
+	return "";
+}
+
+export function checkCookie(): string | null {
+	let user = getCookie("token");
+	if (user !== "") {
+		return user;
+	} else {
+		return null;
+	}
+}
+
+export function setLoginCookie(payload: any): void {
+    setCookie("login_state", "true");
+    setCookie("token_expires_in", payload.expires_in);
+    setCookie("token_access_token", payload.access_token);
+    setCookie("token_refresh_token", payload.refresh_token);
+    setCookie("token_user_name", payload.user_name);
+}
