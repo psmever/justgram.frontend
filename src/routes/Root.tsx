705c056c67965a72c0c8f20358f0c65f0c920caa
psmever <psmever@gmail.com>
2020-02-13 21:50:43 +0900

    코드 정리.

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState } from "react";
+import React, {useEffect, useState } from "react";
 import { Router, Route} from "react-router-dom";
 import Switch from "./Switch";
 import {LoadingPage } from "resources/pages";
@@ -21,16 +21,17 @@ const Root = ({
         checkStart,
     } = useBase(); // 시스템 체크 용 훅 연결.
 
-    const CheckProgress = useMemo(() => checkProgress, [checkProgress]); // 진행률.
-    const [ isLoading, setIsLoading] = useState(true); // 실제 로팅 페이지 보여 줄껀지에 사용할 스테이트
+    // const CheckProgress = useMemo(() => checkProgress, [checkProgress]); // 진행률.
+    const [ isLoading, setIsLoading] = useState(true); // 실제 로딩 페이지 보여 줄껀지에 사용할 스테이트
 
     const profile_active = useSelector((state: RootState) => state.login_state.profile_active);
 
     useEffect(() => {
-        if(CheckProgress === "yet") { // 기본 진행 스테이트가 아직 시작 전에면 체크 훅 사작.
-            checkStart();
-        }
-    });
+        checkStart();
+
+        //TODO: 에디에서 에러나서??
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     // 체크가 모두 끝나고 리덕스 스토어 까지 완료 되었으면 로딩페이지 숨김용 스테이트 업데이트
     useEffect(() => {
