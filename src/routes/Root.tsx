4defb103c0622ee1e72c5c2a5edfd742f17fe311
psmever <psmever@gmail.com>
2020-02-16 21:05:49 +0900

    route switch 제거

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,22 @@
 import React, {useEffect, useState } from "react";
-import { Router, Route} from "react-router-dom";
-import Switch from "./Switch";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {LoadingPage } from "resources/pages";
 import useBase from "hooks/useBase";
 import { useSelector } from 'react-redux';
 import { RootState } from 'modules';
 import history from "routes/History";
+
+import {
+    LoginPage,
+    LogoutPage,
+    RegisterPage,
+    FeedPage,
+    ExplodePage,
+    ProfilePage,
+    EditProfilePage,
+    DefaultPage,
+    TestPage,
+} from "resources/pages";
 
 interface RootProps  {
     Routerhistory: any
@@ -53,7 +64,22 @@ const Root = ({
     } else {
         return (
             <Router history={ Routerhistory }>
-                <Route component={ Switch } />
+                <Switch>
+                    <Route path="/" exact={ true } component={ LoginPage } />
+                    <Route path="/login" exact={ true } component={ LoginPage } />
+                    <Route path="/logout" exact={ true } component={ LogoutPage } />
+                    <Route path="/register" exact={ true } component={ RegisterPage } />
+                    <Route path="/feed" exact={ true } component={ FeedPage } />
+                    <Route path="/explode" exact={ true } component={ ExplodePage } />
+
+                    <Route path="/profile" exact={ true } component={ ProfilePage } />
+                    <Route path="/profile/edit" exact={ true } component={ EditProfilePage } />
+
+                    <Route path="/main" exact={ true } component={ FeedPage } />
+                    <Route path="/default" exact={ true } component={ DefaultPage } />
+                    <Route path="/test" exact={ true } component={ TestPage } />
+                    <Redirect path="*" to="/login" />
+                </Switch>
             </Router>
         );
     }
