2d391d5ad543c4492ea7a081abf54fec8455a28e
psmever <psmever@gmail.com>
2020-04-07 22:27:59 +0900

    클래형에서 함수형으로 변경, 토큰 리프레시 기능 추가.

--- src/lib/GlobalService.ts
+++ src/lib/GlobalService.ts
@@ -2,99 +2,143 @@ import axios ,{
     AxiosInstance,
     AxiosPromise
 } from 'axios';
-import { getCookie } from "lib/Helper";
 
 import * as commonTypes from 'modules/commonTypes';
+import * as Helper from 'lib/Helper';
+import GlobalAlert from 'lib/GlobalAlert';
+import history from 'routes/History';
 
-
-class GlobalService {
-
-    axiosinstance: AxiosInstance;
-
-    constructor() {
-
-        const axiosinstance: AxiosInstance = axios.create({
-            baseURL: process.env.REACT_APP_API_URL,
-            timeout: 20000,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Request-Client-Type": "A02001",
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization" : "Bearer " + getCookie("login_access_token"),
-
-            }
-        });
-
-        this.axiosinstance = axiosinstance;
+export interface tokenRefreshInterface {
+    state: boolean;
+    data?: {
+        token_type?: string;
+        expires_in?: number;
+        access_token?: string;
+        refresh_token?: string;
+        user_name?: string;
     }
+    message?: string;
+}
 
-    promise = <T>(axiosPromise: AxiosPromise): Promise<T> => {
-        return new Promise<T | any>((resolve, reject) => {
-            axiosPromise
-                .then(response => {
-                    resolve({
-                        state: true,
-                        data: response.data
+export interface apiRequest {
+    authType: boolean;
+    method: 'get'|'post'|'delete'|'put';
+    endpoint: string;
+    payload: any
+
+}
+
+const promise = <T>(axiosPromise: AxiosPromise): Promise<T> => {
+    return new Promise<T | any>((resolve, reject) => {
+        axiosPromise.then(response => {
+            resolve({
+                state: true,
+                data: response.data
+            });
+        })
+        .catch(error => {
+            if (error.response) {
+
+                if(error.response.status === 401 || error.response.status === 403) {
+                    Helper.removeLoginInfo();
+                    GlobalAlert.default({
+                        text: error.response.data.error_message,
                     });
-                })
-                .catch(error => {
-                    if (error.response) {
-                        const errorMessage = error.response.data.error_message;
+                    history.push(process.env.PUBLIC_URL + '/login');
+                }
 
-                        if(typeof errorMessage === 'object') {
-                            // console.debug(errorMessage.toString());
-                            // 어떻게 할것 인지?
-                        } else {
-                            resolve({
-                                state: false,
-                                message: (error.response.data.error_message) ? error.response.data.error_message : "처리중 문제가 발생했습니다."
-                            });
-                        }
-                    } else if (error.request) {
-                        resolve({
-                            status: 444,
-                            // message: "The request was made but no response was received"
-                            message: "문제가 발생 했습니다. 잠시루 다시 시도해 주세요. (code:444)"
-                        });
-                    } else {
-                        resolve({
-                            status: 417,
-                            // message: "Something happened in setting up the request that triggered an Error"
-                            message: "문제가 발생 했습니다. 잠시루 다시 시도해 주세요. (code:417)"
-                        });
-                    }
+                const errorMessage = error.response.data.error_message;
+                if(typeof errorMessage === 'object') {
+                    // console.debug(errorMessage.toString());
+                    // 어떻게 할것 인지?
+                } else {
+                    resolve({
+                        state: false,
+                        message: (error.response.data.error_message) ? error.response.data.error_message : "처리중 문제가 발생했습니다."
+                    });
+                }
+            } else if (error.request) {
+                resolve({
+                    status: 444,
+                    message: "서버와의 통신중 문제가 발생했습니다."
                 });
+            } else {
+                resolve({
+                    status: 417,
+                    message: "알수 없는 에러 입니다."
+                });
+            }
         });
-    };
-
-    error(message: string): never {
-        throw new Error(message);
-    };
-
-    init = (method : string, url: string, params: object): Promise<commonTypes.APIResponseType> => {
-
-        switch(method) {
-            case 'get': {
-                return this.promise<commonTypes.APIResponseType>(this.axiosinstance.get(url, params));
-            }
-            case 'post': {
-                return this.promise<commonTypes.APIResponseType>(this.axiosinstance.post(url, params));
-            }
-            case 'put': {
-                return this.promise<commonTypes.APIResponseType>(this.axiosinstance.put(url, params));
-            }
-            case 'delete': {
-                return this.promise<commonTypes.APIResponseType>(this.axiosinstance.delete(url, params));
-            }
-            default:
-                return this.error("Should never get here");
-        }
-
-    };
+    });
 };
 
-export default new GlobalService();
+export const refresh_token = async ()  => {
+
+    const defaultAxiosinstance: AxiosInstance = axios.create({
+        baseURL: process.env.REACT_APP_API_URL,
+        timeout: 20000,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Request-Client-Type": "A02001",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization" : "Bearer " + Helper.getAccessToken(),
+        }
+    });
+
+    await defaultAxiosinstance.post('/api/justgram/v1/token/refresh', {
+        refresh_token: await Helper.getRefreshToken()
+    }).then(async response => {
+        await Helper.setLoginInfoRefresh(response.data);
+        return true;
+    }).catch(error => {
+        console.debug(error);
+        return false;
+    });
+
+    return true;
+}
+
+const error = (message: string): never => {
+    throw new Error(message);
+};
+
+export const start = async ( {authType, method, endpoint, payload}: apiRequest) => {
+    if(authType) {
+        await refresh_token();
+    }
+    const defaultAxiosinstance: AxiosInstance = axios.create({
+        baseURL: process.env.REACT_APP_API_URL,
+        timeout: 20000,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Request-Client-Type": "A02001",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization" : "Bearer " + Helper.getAccessToken(),
+            // "Authorization" : "Bearer " + Helper.getAccessToken(),
+        }
+    });
+
+    switch(method) {
+        case 'get': {
+            return promise<commonTypes.APIResponseType>(defaultAxiosinstance.get(endpoint, payload));
+        }
+        case 'post': {
+            return promise<commonTypes.APIResponseType>(defaultAxiosinstance.post(endpoint, payload));
+        }
+        case 'put': {
+            return promise<commonTypes.APIResponseType>(defaultAxiosinstance.put(endpoint, payload));
+        }
+        case 'delete': {
+            return promise<commonTypes.APIResponseType>(defaultAxiosinstance.delete(endpoint, payload));
+        }
+        default:
+            return error("Should never get here");
+    }
+}
+
+
 
 /**
  * Cloudinary 프로필 이미지 업로드.
