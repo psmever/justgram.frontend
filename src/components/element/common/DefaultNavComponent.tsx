72f83775de694edcacf9ebad86d24edebf3df6d0
psmever <psmever@gmail.com>
2020-05-03 11:20:23 +0900

    상단 링크 변경.

--- src/components/element/common/DefaultNavComponent.tsx
+++ src/components/element/common/DefaultNavComponent.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from 'modules';
 import { useParams } from 'react-router-dom';
-import * as Helper from 'lib/Helper';
 import { NavigationStyle, NavigationNavigationColumn, NavigationColumn, NavigationsLinks, NavigationListItem, NavigationLink } from 'styles';
 import image_logo from 'assets/images/logo.png';
+import * as _ from 'lodash';
 
 interface RouteParams {
     user_name: string
@@ -16,14 +16,19 @@ function DefaultNavComponent() {
 
     const params = useParams<RouteParams>();
 
-    // 로그인 상태가 정상일떄 홈 링크 변경. 로그인 페이지로 보내지 않기 위해.
     const login_state = useSelector((state: RootState) => state.login_state.state);
+
     const login_user_name = useSelector((state: RootState) => state.login_state.user_name);
     const loginState = useMemo(() => login_state, [login_state]);
-    const homeLink = (loginState === "success") ? process.env.PUBLIC_URL + "/feed" : process.env.PUBLIC_URL + "/login";
-    const profileLink = (loginState === "success" && login_user_name) ? process.env.PUBLIC_URL + `/${login_user_name}/profile` : process.env.PUBLIC_URL + "/login";
-    const signLink = (loginState === "success" && login_user_name) ? process.env.PUBLIC_URL + `/logout` : process.env.PUBLIC_URL + "/login";
-    const followersLink = (params.user_name) ? process.env.PUBLIC_URL + `/${params.user_name}/followers` : process.env.PUBLIC_URL + `/${Helper.cookieManager.get('login_user_name')}/followers`;
+
+
+    const isLogined = (loginState === "success" && _.isEmpty(login_user_name) === false) ? true : false;
+
+    const homeLink = process.env.PUBLIC_URL + "/feed";
+    const postLink = isLogined ? process.env.PUBLIC_URL + `/post` : process.env.PUBLIC_URL + "/login";
+    const followersLink = isLogined ? process.env.PUBLIC_URL + `/${params.user_name}/followers` : process.env.PUBLIC_URL + "/login";
+    const profileLink = isLogined ? process.env.PUBLIC_URL + `/${login_user_name}/profile` : process.env.PUBLIC_URL + "/login";
+    const signLink = isLogined ? process.env.PUBLIC_URL + `/logout` : process.env.PUBLIC_URL + "/login";
 
     return (
             <NavigationStyle>
@@ -38,7 +43,7 @@ function DefaultNavComponent() {
                 <NavigationColumn>
                     <NavigationsLinks>
                         <NavigationListItem>
-                            <NavigationLink to={process.env.PUBLIC_URL + "/post"}><i className="fa fa-edit fa-lg"></i></NavigationLink>
+                            <NavigationLink to={postLink}><i className="fa fa-edit fa-lg"></i></NavigationLink>
                         </NavigationListItem>
                         <NavigationListItem>
                             <NavigationLink to={followersLink}><i className="fa fa-heart-o fa-lg"></i></NavigationLink>
@@ -47,7 +52,12 @@ function DefaultNavComponent() {
                             <NavigationLink to={profileLink}><i className="fa fa-user-o fa-lg"></i></NavigationLink>
                         </NavigationListItem>
                         <NavigationListItem>
+                            {isLogined
+                            ?
                             <NavigationLink to={signLink}><i className="fa fa-sign-out fa-lg"></i></NavigationLink>
+                            :
+                            <NavigationLink to={signLink}><i className="fa fa-sign-in fa-lg"></i></NavigationLink>
+                            }
                         </NavigationListItem>
                     </NavigationsLinks>
                 </NavigationColumn>
