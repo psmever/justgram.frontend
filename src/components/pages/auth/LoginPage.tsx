3cb6b823830789afb812d72418edea6f71d0eaf3
psmever <psmever@gmail.com>
2020-11-15 11:59:41 +0900

    [Working] login page.

--- src/components/pages/auth/LoginPage.tsx
+++ src/components/pages/auth/LoginPage.tsx
@@ -6,10 +6,19 @@ import {
     LoginBox,
     LoginLogo,
     LoginForm,
-
+    LoginFormInput,
+    LoginFromButton,
+    LoginDivider,
+    LoginLink,
+    LoginLinkSmall,
+    LoginBoxSignUpLink,
+    LoginBoxTransparent,
+    LoginGoAppStores,
+    LoginGoAppStore,
 } from 'styled/Login';
 
 export default function LoginPage() {
+
 
     return (
         <>
@@ -21,29 +30,27 @@ export default function LoginPage() {
                     <LoginBox>
                         <LoginLogo src={process.env.PUBLIC_URL + '/assets/images/loginLogo.png'}/>
                         <LoginForm>
-                            <input type="text" name="username" placeholder="Username" required />
-                            <input type="password" name="password" placeholder="Password" required />
-                            <input type="submit" value="Log in" />
+                            <LoginFormInput type="text" name="username" placeholder="Username" required />
+                            <LoginFormInput type="password" name="password" placeholder="Password" required />
+                            <LoginFromButton>로그인</LoginFromButton>
                         </LoginForm>
-                        <span className="login__divider">or</span>
-                        <a href="LINK" className="login__link">
+                        <LoginDivider>or</LoginDivider>
+                        <LoginLink to="" onClick={(e) => e.preventDefault()}>
                             <i className="fa fa-money"></i>
-                            Log in with Facebook
-                        </a>
-                        <a href="LINK" className="login__link login__link--small">Forgot password</a>
+                            페이스북으로 로그인
+                        </LoginLink>
+                        <LoginLinkSmall to="" onClick={(e) => e.preventDefault()}>비밀번호가 생각 안나세요?</LoginLinkSmall>
                     </LoginBox>
-                    <div className="login__box">
-                        <span>Don't have an account?</span> <a href="LINK">Sign up</a>
-                    </div>
-                    <div className="login__box--transparent">
+                    <LoginBox>
+                        <span>계정이 없으신가요?</span> <LoginBoxSignUpLink to="" onClick={(e) => e.preventDefault()}>Sign up</LoginBoxSignUpLink>
+                    </LoginBox>
+                    <LoginBoxTransparent>
                         <span>Get the app.</span>
-                        <div className="login__appstores">
-                            <img src="images/ios.png" className="login__appstore" alt="Apple appstore logo"
-                                title="Apple appstore logo" />
-                            <img src="images/android.png" className="login__appstore" alt="Android appstore logo"
-                                title="Android appstore logo" />
-                        </div>
-                    </div>
+                        <LoginGoAppStores>
+                            <LoginGoAppStore src={process.env.PUBLIC_URL + '/assets/images/ios.png'} alt="Apple appstore logo" title="Apple appstore logo" />
+                            <LoginGoAppStore src={process.env.PUBLIC_URL + '/assets/images/android.png'} alt="Android appstore logo" title="Android appstore logo" />
+                        </LoginGoAppStores>
+                    </LoginBoxTransparent>
                 </LoginColumn>
             </LoginMain>
             <footer className="footer">
--- src/styled/Login.ts
+++ src/styled/Login.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
 export const LoginMain = styled.main`
     width: 100%;
@@ -20,6 +21,8 @@ export const LoginColumnImage = styled.img`
 export const LoginBox = styled.div`
     background-color: white;
     border: 1px solid #e6e6e6;
+    padding: 30px 0;
+    text-align: center;
 
     &:first-child {
         margin-bottom: 10px;
@@ -32,5 +35,104 @@ export const LoginLogo = styled.img`
     height: 50px;
 `
 // TODO: From 처리.
-export const LoginForm = styled.form`
+export const LoginForm = styled.div`
+
+`
+
+export const LoginFormInput = styled.input`
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 7px;
+    font-size: 14px;
+    border: 0;
+    border: 1px solid #e6e6e6;
+    border-radius: 5px;
+    background: #fafafa;
+
+    &:focus {
+        outline: none;
+        border: 1px solid #a9a9a9;
+    }
+
+    &:first-child {
+        margin-bottom: 5px;
+    }
+`
+
+export const LoginFromButton = styled.button`
+    background-color: #3f99ed;
+    border: 0;
+    padding: 5px;
+    margin-top: 15px;
+    margin-bottom: 20px;
+    color: white;
+    font-weight: 600;
+    width: 100%;
+    border-radius: 0.5rem;
+`
+
+export const LoginDivider = styled.span`
+    display: block;
+    text-transform: uppercase;
+    font-weight: 600;
+    color: #999;
+    margin-bottom: 20px;
+    position: relative;
+    width: 100%;
+
+    &:before {
+        content: "";
+        height: 1px;
+        background-color: rgba(153, 153, 153, 0.5);
+        width: 40%;
+        position: absolute;
+        left: 0;
+        top: 10px;
+    }
+
+    &:after {
+        content: "";
+        height: 1px;
+        background-color: rgba(153, 153, 153, 0.5);
+        width: 40%;
+        position: absolute;
+        right: 0;
+        top: 10px;
+    }
+`
+
+export const LoginLink = styled(Link)`
+    display: block;
+    text-decoration: none;
+    color: #003569;
+`
+
+export const LoginLinkSmall = styled(Link)`
+    display: block;
+    text-decoration: none;
+    color: #003569;
+    margin-top: 30px;
+`
+
+export const LoginBoxSignUpLink = styled(Link)`
+    text-decoration: none;
+    color: #0070ffbf;
+`
+
+export const LoginBoxTransparent = styled.div`
+    background: none;
+    border: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 35px;
+`
+
+export const LoginGoAppStores = styled.div`
+    margin-top: 20px;
+`
+
+export const LoginGoAppStore = styled.img`
+    height: 40px;
 `
