f511c6eeef35dd6491a035de735f6b3c9af7a4a2
psmever <psmever@gmail.com>
2020-11-15 16:09:46 +0900

    [UPDATE] code 정리.

--- src/components/pages/feed/MainPage.tsx
+++ src/components/pages/feed/MainPage.tsx
@@ -27,6 +27,7 @@ export default function MainPage() {
     return (
         <React.Fragment>
             <MainStyle>
+
                 <MainPhoto>
                     <PhotoHeader>
                         <PhotoAvatar src={process.env.PUBLIC_URL + '/assets/images/avatar.jpg'}/>
@@ -67,6 +68,49 @@ export default function MainPage() {
                         </PhotoAddCommentContainer>
                     </PhotoInfo>
                 </MainPhoto>
+
+                <MainPhoto>
+                    <PhotoHeader>
+                        <PhotoAvatar src={process.env.PUBLIC_URL + '/assets/images/avatar.jpg'}/>
+                        <PhotoUserInfo>
+                            <PhotoAuthor>psmever</PhotoAuthor>
+                            <PhotoLocation>JustGram</PhotoLocation>
+                        </PhotoUserInfo>
+                    </PhotoHeader>
+                    <PhotoBox><PhotoImage src={process.env.PUBLIC_URL + '/assets/images/feedPhoto.jpg'} /></PhotoBox>
+                    <PhotoInfo>
+                        <PhotoActions>
+                            <PhotoAction>
+                                <PhotoHeartIcon size="20px"/>
+                            </PhotoAction>
+                            <PhotoAction>
+                                <PhotoCommentIcon size="23px"/>
+                            </PhotoAction>
+                        </PhotoActions>
+                        <PhotoLikes>45 likes</PhotoLikes>
+                        <PhotoComments>
+                            <PhotoComment>
+                                <PhotoCommentAuthor>serranoarevalo</PhotoCommentAuthor> love this!
+                            </PhotoComment>
+                            <PhotoComment>
+                                <PhotoCommentAuthor>serranoarevalo</PhotoCommentAuthor> love this!
+                            </PhotoComment>
+                            <PhotoComment>
+                                <PhotoCommentAuthor>serranoarevalo</PhotoCommentAuthor> love this!
+                            </PhotoComment>
+                            <PhotoComment>
+                                <PhotoCommentAuthor>serranoarevalo</PhotoCommentAuthor> love this!
+                            </PhotoComment>
+                        </PhotoComments>
+                        <PhotoTimeAgo>2 hours ago</PhotoTimeAgo>
+                        <PhotoAddCommentContainer>
+                            <PhotoAddCommentTextArea placeholder="댓글을 등록해 주세요."></PhotoAddCommentTextArea>
+                            <PhotoAddCommentTextAreaIcon size="20"/>
+                        </PhotoAddCommentContainer>
+                    </PhotoInfo>
+                </MainPhoto>
+
+
             </MainStyle>
         </React.Fragment>
     );
