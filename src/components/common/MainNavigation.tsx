996d59635d31ddb9b48c952d8c52a247ff3b66dd
psmever <psmever@gmail.com>
2020-11-15 14:26:55 +0900

    [ADD] MainNavigation.

--- src/components/common/MainNavigation.tsx
+++ src/components/common/MainNavigation.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Navigation,
+    NavigationColumn,
+    NavigationSearch,
+    NavigationLinks,
+    NavigationListItem,
+    NavigationLink,
+} from 'styled/MainNavigation';
+import { SearchIcon, TopHomeIcon, TopCompassIcon } from 'styled/StyledIcons';
 
 export default function MainNavigation() {
     return (
         <React.Fragment>
-            MainNavigation
+            <Navigation>
+                <NavigationColumn>
+                    <Link to="/feed/main"><img src={process.env.PUBLIC_URL + '/assets/images/logo.png'} alt=""/></Link>
+                </NavigationColumn>
+                <NavigationColumn>
+                    <SearchIcon size="20px" />
+                    <NavigationSearch type="text" placeholder="Search"/>
+                </NavigationColumn>
+                <NavigationColumn>
+                    <NavigationLinks>
+                        <NavigationListItem>
+                            <NavigationLink to="" onClick={(e) => e.preventDefault()}>
+                                <TopHomeIcon size="25px"/>
+                            </NavigationLink>
+                        </NavigationListItem>
+                        <NavigationListItem>
+                            <NavigationLink to="" onClick={(e) => e.preventDefault()}>
+                                <TopCompassIcon size="25px"/>
+                            </NavigationLink>
+                        </NavigationListItem>
+                    </NavigationLinks>
+                </NavigationColumn>
+            </Navigation>
         </React.Fragment>
     );
 }
